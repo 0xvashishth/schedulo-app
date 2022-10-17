3b0f48afc15cd2da33de624d5e68eb9e23241d92
Ayush Rudani <ayushcrudani@gmail.com>
2022-10-17 21:51:34 +0530

    Some comment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:schedulo/data/calculator.dart';
 import 'package:schedulo/data/faq.dart';
 import 'package:schedulo/data/halloffame.dart';
--- lib/pages/dashBoard.dart
+++ lib/pages/dashBoard.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:schedulo/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
