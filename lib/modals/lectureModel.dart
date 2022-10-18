fede119307178573db237935081e6b6e627dfb16
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-10-18 11:52:19 +0530

    mdal edoted

--- lib/modals/lectureModel.dart
+++ lib/modals/lectureModel.dart
@@ -1,24 +1,29 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
 class LectureModel {
   String? subject;
   String? description;
   String? qid;
   String? department;
   String? sem;
-  final DateTime? will_start_at;
-  final DateTime? will_end_at;
-  final Duration? recurringInterval;
-
-  LectureModel(
-      {
-        this.subject,
-        this.qid,
-        this.description,
-        this.department,
-        this.sem,
-        this.will_end_at,
-        this.will_start_at
-      }
-      );
+  int? weekday;
+  TimeOfDay? will_start_at;
+  TimeOfDay? will_end_at;
+  int? duration;
+  // (this.will_end_at.hour * 60 + will_end_at.minute) * 60 - (will_start_at.hour * 60 + will_start_at.minute) * 60,
+  LectureModel({
+    this.subject,
+    this.qid,
+    this.description,
+    this.department,
+    this.sem,
+    this.weekday,
+    this.will_end_at,
+    this.will_start_at,
+    this.duration,
+  });
 
   //data from server
   factory LectureModel.fromMap(map) {
@@ -30,6 +35,8 @@ class LectureModel {
       sem: map['sem'],
       will_start_at: map['will_start_at'],
       will_end_at: map['will_end_at'],
+      weekday: map['weekday'],
+      duration: map['duration'],
     );
   }
 
@@ -42,7 +49,9 @@ class LectureModel {
       'department': department,
       'sem': sem,
       'will_start_at': will_start_at,
-      'will_end_at': will_end_at
+      'will_end_at': will_end_at,
+      'duration': duration,
+      'weekday': weekday,
     };
   }
 }
