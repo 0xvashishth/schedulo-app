c883a9334f81851da451051e8f5537dc9447b55f
Ayush Rudani <ayushcrudani@gmail.com>
2022-10-18 11:48:39 +0530

    Syllabus link updated

--- lib/metadata/api_syllabus.dart
+++ lib/metadata/api_syllabus.dart
@@ -4,7 +4,7 @@ import 'package:path_provider/path_provider.dart';
 
 class ApiSyllabus {
   static String url =
-      'https://www.mrsptu.ac.in/uploads2021/syllabus/MRSPTU%20B.Tech.%20CSE%206th%20sem%20syllabus%202018%20Batch%20onwards.pdf';
+      'https://ddu.ac.in/pdf/BTech_CE_Syllabus_2020_Onwards_SEPT_2022.pdf';
   static Future<String> loadPDF() async {
     var response = await http.get(Uri.parse(url));
     var dir = await getApplicationDocumentsDirectory();
---
+++ lib/services/lecture-service.dart
@@ -0,0 +1,11 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:schedulo/modals/userModals.dart';
+
+// class AuthService {
+//   FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//   static final _auth = FirebaseAuth.instance;
+//   User? user = _auth.currentUser;
